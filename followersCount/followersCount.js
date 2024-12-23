96dfbab173145e631b0d4e41c8f136195b207473
“danikoshvili” <dvass0220@gmail.com>
2024-12-22 23:50:27 -0500

    Count

--- followersCount/followersCount.js
+++ followersCount/followersCount.js
@@ -20,3 +20,9 @@ function checkCountValue() {
         alert("Your Instagram post gained 20 followers! Keep it up!");
     }
 }
+
+function resetCount() {
+    count = 0;
+    alert("The follower count has been reset to 0");
+    displayCount();
+}
