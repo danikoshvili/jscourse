02a1b704afaa23882b13bb9fda8b28e9a4a59506
“danikoshvili” <dvass0220@gmail.com>
2024-12-25 19:37:17 -0500

    Typing speed

--- bookSystem/bookSystem.js
+++ bookSystem/bookSystem.js
@@ -5,6 +5,7 @@ function addBook() {
     const authorName = document.getElementById('authorName').value;
     const bookDescription = document.getElementById('bookDescription').value;
     const pagesNumber = parseInt(document.getElementById('pagesNumber').value);
+    
     if (bookName && authorName && bookDescription && !isNaN(pagesNumber)) {
         const book = {
             name: bookName,
@@ -13,21 +14,29 @@ function addBook() {
             pagesNumber: pagesNumber
         };
         books.push(book);
-        showbooks();
+        showBooks();
         clearInputs();
     } else {
         alert('Please fill in all fields correctly.');
     }
 }
 
-function showbooks() {
-    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+function showBooks() {
+    const booksDiv = books.map((book, index) => `
+        <h1>Book Number: ${index + 1}</h1>
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
-    );
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>
+    `);
     document.getElementById('books').innerHTML = booksDiv.join('');
+}
+
+function deleteBook(index) {
+    // Remove the book at the specified index
+    books.splice(index, 1);
+    showBooks(); // Refresh the list of books
 }
 
 function clearInputs() {
